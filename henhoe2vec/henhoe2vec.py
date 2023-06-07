4559d8b3282e6c01903c41eeef9f1ef8da093d9d
Robert Giesler <robert1020@me.com>
2023-06-07 10:56:32 +0200

    Finished implementing preprocessing of transition probabilities.

--- henhoe2vec/henhoe2vec.py
+++ henhoe2vec/henhoe2vec.py
@@ -1,5 +1,5 @@
 import numpy as np
-import py3plex
+from alias_sampling import alias_setup, alias_draw
 
 
 class HenHoe2vec:
@@ -9,7 +9,7 @@ class HenHoe2vec:
 
     Attributes
     ----------
-    henhoe_network : py3plex multinet
+    henhoe_network : NetworkX Graph
         Heterogeneous-node homogeneous-edge network.
     is_directed : bool
         Whether the network is directed or not.
@@ -33,7 +33,7 @@ class HenHoe2vec:
 
         Parameters
         ----------
-        henhoe_network : py3plex multinet
+        henhoe_network : NetworkX Graph
             Heterogeneous-node homogeneous-edge network.
         is_directed : bool
             Whether the network is directed or not.
@@ -104,6 +104,114 @@ class HenHoe2vec:
             Random walk of length `walk_length` starting at `start_node`.
         """
 
+    def get_node_trans_probs(self, node):
+        """
+        Using alias sampling, calculate the discrete transition probability distribution
+        from node to all its neighbors based on edge weights and switching parameters.
+        Only used for first step of each walk where there is no previous node.
+
+        Parameters
+        ----------
+        node : 2-tuple of str
+            Node for which to get the transition probabilities, e.g., ('n1','l1').
+
+        Returns
+        -------
+        J : list of ints
+            The alias table.
+        q : list of floats
+            The probability table.
+        """
+        N = self.N
+        s = self.s
+
+        # Unnormalized transition probabilites based on edge weights
+        unnormalized_probs = []
+        for nbr in sorted(N.neighbors(node)):
+            src_layer = N.nodes[node]["layer"]
+            trgt_layer = N.nodes[nbr]["layer"]
+            weight = N[node][nbr]["weight"]
+
+            if src_layer == trgt_layer:
+                unnormalized_probs.append(weight)
+            else:
+                if (src_layer, trgt_layer) in s:
+                    switch_param = s[(src_layer, trgt_layer)]
+                else:
+                    switch_param = s["default"]
+                unnormalized_probs.append(weight / switch_param)
+
+        # Normalization constant
+        norm_const = sum(unnormalized_probs)
+        # Normalized transition probabilities
+        normalized_probs = [float(u_prob) / norm_const for u_prob in unnormalized_probs]
+
+        return alias_setup(normalized_probs)
+
+    def get_edge_trans_probs(self, previous, current):
+        """
+        Using alias sampling, calculate the discrete transition probability distribution
+        from `current` to all its neighbors based on edge weights, p, q, and switching
+        parameters. `previous` is the previous node of the walk. Used for all steps of
+        the walk except for the first one.
+
+        Parameters
+        ----------
+        previous : 2-tuple of str
+            Previous node on the random walk, e.g., ('n1','l1').
+        current : 2-tuple of str
+            Current node on the random walk for which we want to calculate the
+            transition probabilities to its neighbors, e.g., ('n2','l1').
+
+        Returns
+        -------
+        J : list of ints
+            The alias table.
+        q : list of floats
+            The probability table.
+        """
+        N = self.N
+        p = self.p
+        q = self.q
+        s = self.s
+
+        # Unnormalized transition probabilites
+        unnormalized_probs = []
+        for nbr in sorted(N.neighbors(current)):
+            src_layer = N.nodes[current]["layer"]
+            trgt_layer = N.nodes[nbr]["layer"]
+            weight = N[current][nbr]["weight"]
+
+            # Neighbor is on the same layer
+            if src_layer == trgt_layer:
+                if nbr == previous:
+                    unnormalized_probs.append(weight / p)  # Return
+                elif N.has_edge(previous, nbr):
+                    unnormalized_probs.append(weight)
+                else:
+                    unnormalized_probs.append(weight / q)  # Explore
+            # Neighbor is on a different layer
+            else:
+                # Get switching parameter
+                if (src_layer, trgt_layer) in s:
+                    switch_param = s[(src_layer, trgt_layer)]
+                else:
+                    switch_param = s["default"]
+
+                if nbr == previous:
+                    unnormalized_probs.append(weight / (p * switch_param))  # Return
+                elif N.has_edge(previous, nbr):
+                    unnormalized_probs.append(weight / switch_param)
+                else:
+                    unnormalized_probs.append(weight / (q * switch_param))  # Explore
+
+        # Normalization constant
+        norm_const = sum(unnormalized_probs)
+        # Normalized transition probabilities
+        normalized_probs = [float(u_prob) / norm_const for u_prob in unnormalized_probs]
+
+        return alias_setup(normalized_probs)
+
     def preprocess_transition_probs(self):
         """
         Preprocessing of transition probabilities for guiding the random walks.
@@ -113,3 +221,27 @@ class HenHoe2vec:
 
         transition_probs_nodes = {}
         transition_probs_edges = {}
+
+        # Calculate the transition probabilities for the first step of the walks
+        for node in N.nodes:
+            transition_probs_nodes[node] = self.get_node_trans_probs(node)
+
+        # Calculate the transition probabilities for all other steps of the walks
+        if is_directed:
+            for edge in N.edges:
+                transition_probs_edges[edge] = self.get_edge_trans_probs(
+                    edge[0], edge[1]
+                )
+        else:
+            for edge in N.edges:
+                transition_probs_edges[edge] = self.get_edge_trans_probs(
+                    edge[0], edge[1]
+                )
+                transition_probs_edges[(edge[1], edge[0])] = self.get_edge_trans_probs(
+                    edge[1], edge[0]
+                )
+
+        self.transition_probs_nodes = transition_probs_nodes
+        self.transition_probs_edges = transition_probs_edges
+
+        return
